52b2a6e9968dae8ba8e44bcca28725261ce290ad
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-07-23 22:13:51 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,11 +1,19 @@
 package com.educandoweb.course.entities;
 
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+//Nome da tabela no banco de dados
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
